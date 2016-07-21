b985c95c1a2f3f220eecea2eb744f0d8a89c52e9
Edward Knowles <mail@eknowles.com>
2016-07-21 14:46:01 +0100

    add price options and resolve postcodes

--- cmds/price.js
+++ cmds/price.js
@@ -1,11 +1,34 @@
 'use strict';
 
+var request = require('request');
+var address = require('../lib/address');
+
+const SANDBOX_URL = 'https://sandbox.api.addisonlee.com/v2-sandbox/quickbook/quote/price';
+const LIVE_URL = 'https://api.addisonlee.com/v2/quickbook/quote/price';
+
 module.exports = function (program) {
+
   program
     .command('price [locations]')
+    .option('-t, --tomorrow', 'Set the pickup time for tomorrow')
+    .option('-k, --key <apikey>', 'Set the API Key for this request')
+    .option('-c, --cash', 'Set the payment method to Cash')
+    .option('-C, --credit-card', 'Set the payment method to Credit Card')
+    .option('-s, --sandbox', 'Use the sandbox env of MuleSoft')
     .version('0.0.0')
     .description('Get a price for a journey')
-    .action(function (locations) {
-      locations = locations || 'NW13ER,TW61EW';
+    .action(function (locations, pr) {
+      const LOC = locations || 'NW13ER,TW61EW';
+      const URL = pr.sandbox ? SANDBOX_URL : LIVE_URL;
+      const QUOTE = {
+        locations: [],
+        payment_method: 'Account',
+        services: [
+          { code: 'OneFourPassengers' }
+        ]
+      };
+
+      address.getLatLongFromPostcodes(LOC.split(','));
+
     });
 };
